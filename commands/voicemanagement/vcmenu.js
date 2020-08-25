6b3a20dbfc29d52e21fe352abeeef0c4e8928f80
SkyExit <60986116+SkyExit@users.noreply.github.com>
2020-08-25 11:46:17 +0200

    Create vcmenu.js

---
+++ commands/voicemanagement/vcmenu.js
@@ -0,0 +1,18 @@
+{
+  "name": "vcmenu",
+  "permissions": "NONE",
+  "restriction": "1",
+  "_id": "SqPzM",
+  "actions": [
+    {
+      "channel": "0",
+      "varName": "",
+      "message": "You need to create a voice channel called \"Mechanic VC\".\n\nconnect to it, and a new channel wil be created. If you leave it, it will get deleted.\n\n**&vcinvite <@mention>** - To invite someone.\n**&vckick <@mention>** - To kick him out of your vc.\n**&vcopen** - To make your vc public!\n**&vcclose** - To make your vc private!",
+      "storage": "0",
+      "varName2": "",
+      "iffalse": "0",
+      "iffalseVal": "",
+      "name": "Send Message"
+    }
+  ]
+}
