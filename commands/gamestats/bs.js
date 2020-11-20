765971f6a4b78db287076c842758c841bc0ace07
SkyExit <60986116+SkyExit@users.noreply.github.com>
2020-11-20 19:45:01 +0100

    Create bs.js

---
+++ commands/gamestats/bs.js
@@ -0,0 +1,49 @@
+{
+  "name": "bs",
+  "permissions": "NONE",
+  "restriction": "1",
+  "_id": "tRYsH",
+  "actions": [
+    {
+      "member": "1",
+      "varName": "",
+      "info": "21",
+      "storage": "1",
+      "varName2": "mem",
+      "name": "Store Member Info"
+    },
+    {
+      "member": "1",
+      "varName": "",
+      "info": "16",
+      "storage": "1",
+      "varName2": "avat",
+      "name": "Store Member Info"
+    },
+    {
+      "info": "0",
+      "infoIndex": "1",
+      "storage": "1",
+      "varName": "tagPO",
+      "name": "Store Command Params"
+    },
+    {
+      "code": "const Brawlstars = require(\"brawlstars.js\")\nconst Discord = require('discord.js');\nconst token  = \"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjRkNzMyNWJiLWZhNTQtNGQ0NC1hMDY5LTA3N2ExYTVmNWFiZiIsImlhdCI6MTU5MzYxNDAxMCwic3ViIjoiZGV2ZWxvcGVyL2E2NjMzNTI2LWE2NzItNWQ3Ny1iZDE4LTNjY2Y0Y2Q3ZTA4MyIsInNjb3BlcyI6WyJicmF3bHN0YXJzIl0sImxpbWl0cyI6W3sidGllciI6ImRldmVsb3Blci9zaWx2ZXIiLCJ0eXBlIjoidGhyb3R0bGluZyJ9LHsiY2lkcnMiOlsiMjU1LjI1NS4yNTUuMCIsIjE2NC4xMzIuMTEwLjEzOCJdLCJ0eXBlIjoiY2xpZW50In1dfQ.p0Vr5MmCuXEBxJ1Tx1gHaVSGDdoHwhnUoD13dfFO2EpQE1u3i_QKTFRQNhtO5hb1D-1EnSze93JFfP93eAA--w\"\nconst client = new Brawlstars.Client(token)\n\n;(async () => {\n    const player = await client.getPlayer(tempVars(\"tagPO\"))\n\n    const exampleEmbed = new Discord.MessageEmbed()\n\t.setColor(\"#ffb900\")\n\t.setTitle(player.name + \"'s BS-Stats\")\n\t.setDescription('Some description here')\n\t.setThumbnail('https://vignette2.wikia.nocookie.net/brawlstars/images/1/1c/Brawl_Icon.png/revision/latest?cb=20170621183335')\n\t.addFields(\n\t\t{ name: 'trophies:', value: player.trophies, inline: true },\n        { name: 'highest trophies:', value: player.highestTrophies, inline: true },\n        { name: 'level:', value: player.expLevel, inline: true },\n        { name: 'name:', value: player.name, inline: true },\n        { name: 'expPoints:', value: player.expPoints, inline: true },\n        { name: 'powerPlayPoints:', value: player.powerPlayPoints, inline: true },\n        { name: 'highestPowerPlayPoints:', value: player.highestPowerPlayPoints, inline: true },\n        { name: 'brawlerCount:', value: player.brawlerCount, inline: true },\n        { name: 'soloVictories:', value: player.soloVictories, inline: true },\n        { name: 'duoVictories:', value: player.duoVictories, inline: true },\n        { name: 'trioVictories:', value: player.trioVictories, inline: true },\n        { name: 'totalVictories:', value: player.totalVictories, inline: true },\n\t)\n\t.setTimestamp()\n\t.setFooter('Requested by ' + tempVars(\"mem\"), tempVars(\"avat\"));\n\nmsg.channel.send(exampleEmbed);\n})()",
+      "behavior": "0",
+      "interpretation": "1",
+      "storage": "0",
+      "varName": "",
+      "VTypeSelect": "0",
+      "CVTypeValue": "",
+      "name": "Run Script"
+    }
+  ],
+  "comType": "0",
+  "customData": {
+    "Command Info": {
+      "category": "Gaming",
+      "description": "Get your BrawlStars stats!",
+      "include": "Yes"
+    }
+  }
+}
